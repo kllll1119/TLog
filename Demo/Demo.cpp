c8ab34758ab6ff4b0f70d8d0e04ab0f84cf25718
”tanglei“ <tanglei@xiaoduotech.com>
2022-11-11 17:38:50 +0800

    优化解密工具及算法

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -35,9 +35,9 @@ int main(int argc, char *argv[])
 
 	TLog_Print(u8"这是一个测试\
 		int TEXPORT_API GetPasswordStrength(string password); ", TLOG_LV_INFO);
-	TLog_Print(u8R"({"account":[{"avatar":"http://wework.qpic.cn/bizmail/9wt86APtVAia0L4PdzLONQXLibk5ao578hWp8iaTF5IYjCPk4EUS9MVXA/0","client_mode":0,"company_name":"成都晓多科技有限公司","id":1688850456160424,"nick":"唐磊","phone":"13982104923","pid":4028,"power_enter_mp":false,"user_id":"155960420","work_mode":0}],"account_count":1}
+	TLog_Print(u8R"({"account":[{"avatar":"http://wew23.png","client_mode":0,"id":123,"nick":"sss","phone":"2323","pid":4028,"user":"15293220","mode":0}],"account_count":1}
 	)", TLOG_LV_INFO);
-	TLog_Print(u8R"([accountmanagewnd.cpp:896 (15132) LV_INFO] DelAccountInfo:{"id":1688850456160424,"user_id":"155960420"})", TLOG_LV_INFO);
+	TLog_Print(u8R"([accountmanagewnd.cpp:896 (15132) LV_INFO] DelAccountInfo:{"id":2323,"user_id":"996"})", TLOG_LV_INFO);
 	
 	system("pause");
 
